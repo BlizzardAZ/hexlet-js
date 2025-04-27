65dd005378b4dcc2c6568da3e923e3a6ce1fce22
Alina Zauer <sauer6699@gmail.com>
2025-04-27 14:54:07 +0500

    update index.js package-lock.json package.json

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-console.log('Hello, Hexlet!')
+console.log("Hello, Hexlet!");
 import _ from "lodash";
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
+
+const name = "James";
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+sayHelloLinting("James");
